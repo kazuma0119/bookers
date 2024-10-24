636eac2caddc6f7f7843471eec8193f09ee6ca60
EC2 Default User <ec2-user@ip-172-31-28-137.ap-northeast-1.compute.internal>
2024-10-24 09:49:14 +0000

    [update]バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,14 @@
 class BooksController < ApplicationController
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    flash[:notice] = "Book was successfully created."
-    # 4. トップ画面へリダイレクト
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+       @books=Book.all
+      render :index
+    end
   end
   def index
       @book=Book.new
@@ -23,8 +25,12 @@ class BooksController < ApplicationController
 
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+       redirect_to book_path(@book.id)
+     else
+        @books=Book.all
+       render :edit
+     end
   end
 
   def destroy
